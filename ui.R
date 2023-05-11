d68dcd1e6589867ea8e550941c00b21685f5910a
maria <m.pikoula@ucl.ac.uk>
2023-05-11 18:58:01 +0300

    added hover-over text

--- ui.R
+++ ui.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(shinyBS)
 
 # Define UI for application.
 
@@ -15,14 +16,21 @@ library(shiny)
         headerPanel("Upper bounds of sensitivity and specificity given Cohen's kappa"),
         sidebarPanel(
             numericInput('sens', 'True Biomarker sensitivity', 1, min = 0, max = 1, step=0.005), # step needs to be half of desired (?bug)
+            bsTooltip(id = "sens", title = "hypothetical value of true biomarker sensitivity"),
             numericInput('spec', 'True Biomarker specificity', 1, min=0, max= 1, step=0.005),
+            bsTooltip(id = "spec", title = "hypothetical value of true biomarker specificity"),
             numericInput('kappa', 'kappa value', 0.5, min = 0, max = 1, step=0.005),
+            bsTooltip(id = "kappa", title = "Cohen's kappa value for given diagnosis"),
             h3("Observed performance for specific biomarker\n"),
             " ",
             numericInput('cases', 'Number of cases', NA, min = 0, step=1),
+            bsTooltip(id = "cases", title = "number of cases in the study"),
             numericInput('controls', 'Number of controls', NA, min=0, step=1),
+            bsTooltip(id = "controls", title = "number of controls in the study"),
             numericInput('osens', 'Experimental sensitivity', NA, min = 0, max = 1, step=0.005), # step needs to be half of desired (?bug)
-            numericInput('ospec', 'Experimental specificity', NA, min=0, max= 1, step=0.005)
+            bsTooltip(id = "osens", title = "Experimentally observed sensitivity for the study"),
+            numericInput('ospec', 'Experimental specificity', NA, min=0, max= 1, step=0.005),
+            bsTooltip(id = "ospec", title = "Experimentally observed specificity for the study")
         ),
         
         mainPanel(tags$style(type="text/css",
